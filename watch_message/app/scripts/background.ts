6a64ec1b873f3a9b2afb394d3e39cf59da37b641
nagaminenot <nagamine@slogan.jp>
2018-12-08 18:56:57 +0900

    ハングアウト・Slackの情報を横取りできるようにした

--- watch_message/app/scripts/background.ts
+++ watch_message/app/scripts/background.ts
@@ -1,8 +1,18 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-chrome.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion);
+chrome.runtime.onInstalled.addListener(details => {
+  console.log("previousVersion", details.previousVersion);
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(detail) {
+    console.dir(detail.requestBody);
+  },
+  {
+    urls: ["*://*.slack.com/*", "*://chat.google.com/*"]
+  },
+  ["requestBody"]
+);
 
 console.log(`'Allo 'Allo! Event Page`);
